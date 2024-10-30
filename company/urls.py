590f1d897d761ecd931050897c8da211c2967865
srisudhatri <113518285+srisudhatri@users.noreply.github.com>
2024-10-30 18:34:00 +0530

    30-10-2024

--- company/urls.py
+++ company/urls.py
@@ -3,6 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('',index,name="index"),
+    path('index',index,name="index"),
+
     path('about_us',about_us,name="about_us"),
     path('services',services,name="services"),
 
