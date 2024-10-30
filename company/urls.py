26ec745e6cc77af0f904f90aa6f39bdbd85be3fd
srisudhatri <113518285+srisudhatri@users.noreply.github.com>
2024-10-30 09:36:35 +0530

    30-10-2024

--- company/urls.py
+++ company/urls.py
@@ -4,7 +4,10 @@ from .views import *
 urlpatterns = [
     path('',index,name="index"),
     path('about_us',about_us,name="about_us"),
+    path('services',services,name="services"),
+
 
     
 
 ]
+
--- company/views.py
+++ company/views.py
@@ -7,3 +7,7 @@ def index(request):
 
 def about_us(request):
     return render(request,'about_us.html')
+
+
+def services(request):
+    return render(request,'services.html')
